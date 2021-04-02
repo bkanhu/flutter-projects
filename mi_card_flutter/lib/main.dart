28ebe9d94c9fb58626b49422ed95aa7d2b565a20
bkanhu <iamkanhuc@gmail.com>
2021-04-03 00:19:50 +0530

    mi_card_flutter manually create

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Container(
+            child: Text('kadel'),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
